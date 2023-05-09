b98aebc926901569f9b1bb41cfbf0b8434338056
fjronero99 <70355690+fjronero99@users.noreply.github.com>
2023-05-09 18:41:56 +0000

    in progress

---
+++ Graph.py
@@ -0,0 +1,19 @@
+# # Graph in progress
+# import matplotlib.pyplot as plt
+
+# def plot_xy_graph(x, y):
+#     # create the plot
+#     plt.plot(x, y)
+
+#     # set the labels for the x and y axis
+#     plt.xlabel('X-axis')
+#     plt.ylabel('Y-axis')
+
+#     # show the plot
+#     plt.show()
+
+
+# x = [1, 2, 3, 4, 5]
+# y = [2, 4, 6, 8, 10]
+
+# plot_xy_graph(x, y)
